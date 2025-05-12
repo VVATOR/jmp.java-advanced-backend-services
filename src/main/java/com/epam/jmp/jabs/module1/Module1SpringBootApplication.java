2acc34f2b3de99065316afd17c1c05d9030e1223
Vitali Vikhliayeu <Vitali_Vikhliayeu@epam.com>
2025-05-12 12:40:12 +0300

    module-1: init commit

---
+++ src/main/java/com/epam/jmp/jabs/module1/Module1SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.epam.jmp.jabs.module1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module1SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module1SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/jmp/jabs/module1/Module1SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.jmp.jabs.module1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
